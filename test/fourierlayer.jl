c0dd96a2687a915ca6ec8357c9842677ac16331f
Patrick Zimbrod <76100656+pzimbrod@users.noreply.github.com>
2021-06-29 09:35:48 +0200

    add test routine

---
+++ test/fourierlayer.jl
@@ -0,0 +1,13 @@
+using Test, Random
+import Flux: activations
+
+@testset "FourierLayer" begin
+    # Test the proper construction
+    @test size(FourierLayer(10, 100).weight_f) == (6, 6)
+    @test size(FourierLayer(10, 100).weight_l) == (100, 10)
+    @test size(FourierLayer(10, 100).bias_f) == (6,)
+    @test size(FourierLayer(10, 100).weight_l) == (100,)
+    # Accept only Int as architecture parameters
+    @test_throws MethodError FourierLayer(10, 10.5)
+    @test_throws MethodError FourierLayer(10, 10.5, tanh)
+end
