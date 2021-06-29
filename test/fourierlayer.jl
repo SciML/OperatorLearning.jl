98711add30b5a1cfacfd2d9f083c736aceb286a6
Patrick Zimbrod <76100656+pzimbrod@users.noreply.github.com>
2021-06-29 09:47:22 +0200

    correct an error in fourierlayer test

--- test/fourierlayer.jl
+++ test/fourierlayer.jl
@@ -6,7 +6,7 @@ import Flux: activations
     @test size(FourierLayer(10, 100).weight_f) == (6, 6)
     @test size(FourierLayer(10, 100).weight_l) == (100, 10)
     @test size(FourierLayer(10, 100).bias_f) == (6,)
-    @test size(FourierLayer(10, 100).weight_l) == (100,)
+    @test size(FourierLayer(10, 100).bias_l) == (100,)
     # Accept only Int as architecture parameters
     @test_throws MethodError FourierLayer(10, 10.5)
     @test_throws MethodError FourierLayer(10, 10.5, tanh)
