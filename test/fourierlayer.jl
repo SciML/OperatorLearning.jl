75c095e1722eef904463e138aa53065f5e6057d6
pzimbrod <patrick.zimbrod@informatik.uni-augsburg.de>
2021-11-09 15:58:34 +0100

    fix Wl dim in test & update constructor in burgers

--- test/fourierlayer.jl
+++ test/fourierlayer.jl
@@ -3,7 +3,7 @@ using Test, Random, Flux
 @testset "FourierLayer" begin
     # Test the proper construction
     @test size(FourierLayer(128, 64, 100, 20).Wf) == (128, 64, 51)
-    @test size(FourierLayer(128, 64, 100, 20).Wl) == (64, 128, 1)
+    @test size(FourierLayer(128, 64, 100, 20).Wl) == (64, 128)
     #@test size(FourierLayer(10, 100).bias_f) == (51,)
     #@test size(FourierLayer(10, 100).bias_l) == (100,)
 
