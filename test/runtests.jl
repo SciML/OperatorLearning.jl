474c705417d1364b5739276db7ce1e9fb088564d
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-02-10 06:29:39 -0500

    Fix up tests and dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
+using Pkg
+const LONGER_TESTS = false
+const GROUP = get(ENV, "GROUP", "All")
+
 using OperatorLearning
 using Test
 using Random
 
 Random.seed!(0)
 
+if GROUP == "All" || GROUP == "Core"
 @testset "FourierLayer" begin
     include("fourierlayer.jl")
 end
@@ -14,4 +19,9 @@ end
 
 @testset "Weights" begin
     include("complexweights.jl")
+end
+end
+
+if GROUP == "GPU"
+    # Add GPU Tests Here
 end
